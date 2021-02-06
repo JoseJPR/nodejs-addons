53820520090f29c86331b6f0b3adfd6bf953865f
JoseJPR <me@josejpr.com>
2021-02-06 22:06:54 +0100

    Improve documentation

--- src/hello-word/example01/example.js
+++ src/hello-word/example01/example.js
@@ -1,5 +1,8 @@
+// Import Addon dependencies.
 const addon = require('./addon/build/Release/addon');
 
+// Instance method.
 const runAddon = () => addon.helloWorld();
 
+// Run function.
 runAddon();
--- src/hello-word/example02/example.js
+++ src/hello-word/example02/example.js
@@ -1,8 +1,11 @@
 /**
  * More info about include colors: https://stackoverflow.com/questions/9158150/colored-output-in-c/9158263
  */
+// Import Addon dependencies.
 const addon = require('./addon/build/Release/addon');
 
+// Instance method.
 const runAddon = () => addon.helloWorld();
 
+// Run function.
 runAddon();
